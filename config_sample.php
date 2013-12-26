17d89e08227ff6a05a4a7c10d0908e5691ffd407
datagutten <datagutten@datagutten.net>
2013-12-26 19:34:37 +0100

    First commit of core scripts

---
+++ config_sample.php
@@ -0,0 +1,3 @@
+<?Php
+$outpath='/home/tv';
+?>
---
+++ xmltvgenerator.php
@@ -0,0 +1,60 @@
+<?Php
+class xmltvgenerator
+{
+	public $channel;
+	public $lang;
+	public $outpath;
+
+	function __construct($channel,$lang,$outpath)
+	{
+		$this->xml=new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE tv SYSTEM "xmltv.dtd"><tv generator-info-name="quadepg"/>');
+		$this->channel=$channel;
+		$this->lang=$lang;
+		$this->outpath=$outpath;
+	}
+	function program($programtitle,$description,$start,$stop=false)
+	{
+		$programme=$this->xml->addChild('programme');
+		$programme->addAttribute('start',date('YmdHis O',$start));
+		if($stop!==false)
+			$programme->addAttribute('stop',date('YmdHis O',$stop));
+		$programme->addAttribute('channel',$this->channel);
+		$title=$programme->addChild('title',$programtitle);
+		$title->addAttribute('lang',$this->lang);
+		$desc=$programme->addChild('desc',$description);
+		$desc->addAttribute('lang',$this->lang);
+		return $programme;
+	}
+	function episodeinfo($programme,$season,$current,$total=false,$onscreen=false)
+	{
+		$xmltv_ns=$season-1;
+		$xmltv_ns.='.';
+		$xmltv_ns.=$current-1;
+		if($total!==false)
+			$xmltv_ns.='/'.$total;
+		$xmltv_ns.='.';
+		
+		$episode_num=$programme->addChild('episode-num',$xmltv_ns);
+		$episode_num->addAttribute('system','xmltv_ns');
+		if($onscreen!==false)
+		{
+			$episode_num=$programme->addChild('episode-num',$onscreen);
+			$episode_num->addAttribute('system','onscreen');
+		}
+
+	}
+	function output()
+	{
+		$dom = new DOMDocument('1.0');
+		$dom->preserveWhiteSpace = false;
+		$dom->formatOutput = true;
+		$dom->loadXML($this->xml->asXML());
+		return $dom->saveXML();
+	}
+	function writefile($channel,$date)
+	{
+		$ymd=date('Y-m-d',$date);
+		file_put_contents($this->outpath."/$channel/{$channel}_$ymd.xml",$this->output());
+	}
+}
