3ce270cf7123d7f8349a6117977d94729209a386
datagutten <datagutten@datagutten.net>
2013-12-27 12:12:05 +0100

    Added grabber for Disney Channel XD Norway (xd.disneychannel.no)

---
+++ grabbers/xd.disneychannel.no.php
@@ -0,0 +1,35 @@
+<?Php
+require '../xmltvgenerator.php';
+require '../class.php';
+
+if(isset($argv[1]))
+	$timestamp=strtotime($argv[1]);
+else
+	$timestamp=strtotime('today');
+
+$grabber=new grabber;
+
+$data=$grabber->download('http://disneyxd.disney.no/timeplan/tdxd.xml','xd.disneychannel.no','xml');
+$xml=simplexml_load_string($data);
+
+foreach($xml->SCHEDULE as $day)
+{
+	$xmltv=new xmltvgenerator('xd.disneychannel.no','nb',$grabber->outpath);
+	$date=$day->attributes();
+	$date=preg_replace('^([0-9]{2})/([0-9]{2})/([0-9]{4})^','$3-$2-$1',$date['startdate']); //Rewrite the date so strtotime understands it
+	$timestamp=strtotime($date);
+	//print_r($day->DAY);
+
+	foreach($day->DAY->SLOT as $slot)
+	{
+		$slot=$slot->attributes();
+		$program=$xmltv->program((string)$slot['prog'],(string)$slot['synopsis'],strtotime($slot['time'],$timestamp));
+		//print_r($slot);
+		//var_dump($slot['prog']);
+	}
+	$xmltv->writefile('xd.disneychannel.no',$timestamp);
+	echo date('Y-m-d',$timestamp)."\n";
+	unset($xmltv);
+	//print_r($week->DAY);
+	//break;
+}
