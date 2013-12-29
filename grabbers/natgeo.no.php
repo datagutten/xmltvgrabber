8e3926ac38ee4bed105c42784230a7184b0aa280
datagutten <datagutten@datagutten.net>
2013-12-29 19:10:59 +0100

    Rewritten grabber to use xml class

--- grabbers/natgeo.no.php
+++ grabbers/natgeo.no.php
@@ -8,48 +8,40 @@ else
 	$timestamp=strtotime('today');
 
 $grabber=new grabber;
+$xmltv=new xmltvgenerator('natgeo.no','nb',$grabber->outpath);
 
 $urldate=date('dmy',$timestamp);
+$ymd=date('Y-m-d',$timestamp);
 
 $data=$grabber->download('http://natgeotv.com/no/listings/ngc/'.$urldate,'natgeo.no','htm',$timestamp);
 
 $outfile='/mnt/web/tv/natgeo.no/natgeo.no_'.date('Y-m-d',$timestamp).'.xml';
 
-if(file_exists($outfile))
-	echo "Data for angitt dag er allerede lastet ned\n";
-else
+preg_match_all('^ScheduleDayRow.+ScheduleDayHour"\>([0-9]+:[0-9]+)\<.+/ul^Us',$data,$result);
+	
+foreach ($result[0] as $key=>$program)
 {
-	
-	preg_match_all('^ScheduleDayRow.+ScheduleDayHour"\>([0-9]+:[0-9]+)\<.+/ul^Us',$data,$result);
-	
-	//print_r($result);
-	$output= '<?xml version="1.0" encoding="UTF-8"?>'."\n";
-	$output.= "<!DOCTYPE tv SYSTEM \"xmltv.dtd\">\n\n";
-	$output.= "<tv generator-info-name=\"quadtv\">\n";
-	foreach ($result[0] as $key=>$program)
+	if(preg_match('^\<span class=.Bold.+/span\>^',$program,$title))
 	{
-		preg_match('^\<span class=.Bold.+/span\>^',$program,$title);
-		//if($title[0]=='')
-		{
-		//echo $program."\n";
 		$title=strip_tags($title[0]);
 		$title=str_replace('&','&amp;',$title);
 		echo $result[1][$key].' '.$title."\n";
 		$starttime=str_replace(':','',$result[1][$key]);
+		$starttimestamp_temp=strtotime($ymd.' '.$result[1][$key]);
+		if(isset($starttimestamp) && $starttimestamp_temp<$starttimestamp)
+			$starttimestamp=$starttimestamp_temp+86400; //If the current time is earlier than the previous, increase the date
+		else
+			$starttimestamp=$starttimestamp_temp;
+	
 		if(isset($result[1][$key+1]))
 			$stoptime=str_replace(':','',$result[1][$key+1]);
 		else
 			$stoptime='';
-		$date=date('Ymd',$timestamp);
-		$output.="  <programme start=\"$date{$starttime}00 +0100\" stop=\"$date{$stoptime}00 +0100\" channel=\"natgeo.no\">\n";
-		$output.="    <title lang=\"nb\">$title</title>\n";
-		$output.= "  </programme>\n";
-		
-			
-		}
+
+		$programme=$xmltv->program($title,'',$starttimestamp);
 	}
-	$output.='</tv>';
-	//echo $output;
-	file_put_contents($outfile,$output);
-	echo "\n<br>$outfile<br>\n";
 }
+
+$filename=$xmltv->savefile($timestamp);
+echo "\n$filename\n";
+?>
