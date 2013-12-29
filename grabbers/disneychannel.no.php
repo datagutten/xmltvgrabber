028301171073f6ac01f9b897a9cdfa57d9a9db05
datagutten <datagutten@datagutten.net>
2013-12-29 21:31:15 +0100

    Rewritten grabber with better xml handling

--- grabbers/disneychannel.no.php
+++ grabbers/disneychannel.no.php
@@ -1,69 +1,44 @@
 <?php
+require_once '../class_xmltvgenerator.php';
 require_once '../class_grabber.php';
-
-if(isset($argv[1]))
-	$timestamp=strtotime($argv[1]);
-else
-	$timestamp=strtotime('today');
-	
 $grabber=new grabber;
-
-$data=$grabber->download('http://www.disney.no/DisneyChannel/binary/tvguide/tdc.xml','disneychannel.no','xml',$timestamp);
-$xml=simplexml_load_string($data);
-//$xml=simplexml_load_file('rawdata/disneychannel.no/disneychannel.no_1362524400.xml');
-$xml=json_decode(json_encode($xml),true);
-//print_r($xml);
-//echo date('H:i',$time);
-//die();
-foreach ($xml['SCHEDULE'] as $week)
+define('channel','disneychannel.no');
+if(isset($argv[1]))
 {
-	$date=$week['@attributes']['startdate'];
-	
-	$year=substr($date,6,4);
-	$month=substr($date,3,2);
-	$day=substr($date,0,2);
-	$startday=strtotime("$year/$month/$day");
-	
-	//$daykey=5; //Min 0 I dag 5 Max 6
-	for ($daykey=0; $daykey<=6; $daykey++)
-	{
-			$output= '<?xml version="1.0" encoding="UTF-8"?>'."\n";
-			$output.= "<!DOCTYPE tv SYSTEM \"xmltv.dtd\">\n\n";
-		
-			$output.= "<tv generator-info-name=\"quadtv\">\n";
-		
-		
-			
-		$currentday=strtotime('+'.$daykey.' days',$startday);
-		//print_r($xml);
-		foreach ($xml['SCHEDULE'][0]['DAY'][$daykey]['SLOT'] as $key=>$program)
-		{
-			if(!isset($xml['SCHEDULE'][0]['DAY'][$daykey]['SLOT'][$key+1]))
-			$stoptime='0123';
-			else
-			$stoptime=str_replace(':',NULL,$xml['SCHEDULE'][0]['DAY'][$daykey]['SLOT'][$key+1]['@attributes']['time']);
-			
-			$program=$program['@attributes'];
-			
-			$starttime=str_replace(':',NULL,$program['time']);
-			$day=date('Ymd',$currentday);
-			//$program['prog']=htmlentities($program['prog']);
-			$program['prog']=str_replace('&',NULL,$program['prog']);
-			$output.="  <programme start=\"$day{$starttime}00 +0100\" stop=\"$day{$stoptime}00 +0100\" channel=\"disneychannel.no\">\n";
-			$output.="    <title lang=\"nb\">{$program['prog']}</title>\n";
-			$output.= "    <category lang=\"en\">Children's</category>\n";
-			$output.= "  </programme>\n";
-		
-		}
-		$output.='</tv>';
-		$outfile=$grabber->outpath.'disneychannel.no/disneychannel.no_'.date('Y-m-d',$currentday).'.xml';
-
-		if(!file_exists($outfile))
-			file_put_contents($outfile,$output);
-		echo $outfile."<br>\n";
-		unset($outfile,$output);
-	}
+	$timestamp=strtotime($argv[1]);
+	if(!$data=$grabber->getlocalfile(channel,'xml',$timestamp))
+		return;
 }
-//echo $output;
+else
+{
+	$timestamp=strtotime('today');
+	$data=$grabber->download('http://www.disney.no/DisneyChannel/binary/tvguide/tdc.xml',channel,'xml',$timestamp);
+}
+
+$xml=simplexml_load_string($data);
+foreach($xml->SCHEDULE as $week)
+{
+	$startdate=$week->attributes();
+	$startdate=preg_replace('^([0-9]{2})/([0-9]{2})/([0-9]{4})^','$3-$2-$1',(string)$startdate); //Rewrite the date so strtotime understands it
+	echo $startdate."\n";
+	$timestamp_week=strtotime($startdate);
+	foreach($week->DAY as $day)
+	{
+		$xmltv=new xmltvgenerator(channel,'nb',$grabber->outpath);
+		$dayinfo=$day->attributes();
+		$timestamp_day=strtotime($dayinfo['name'],$timestamp_week);
+		foreach($day->SLOT as $slot)
+		{
+			$programinfo=$slot->attributes();
+			if($slot['prog']=='MOVIE: TBC')
+				$title='Film: '.substr($slot->FILM->attributes()['title'],2);
+			else
+				$title=(string)$slot['prog'];
+			$program=$xmltv->program($title,(string)$slot['synopsis'],strtotime($slot['time'],$timestamp_day));
+		}
+		$filename=$xmltv->savefile($timestamp_day);
+		echo "$filename\n";
+		unset($xmltv);
+	}	
+}
 ?>
-	
