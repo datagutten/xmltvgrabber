b464b8268231b47ed7b3a6abf305fac509d73935
datagutten <datagutten@datagutten.net>
2020-04-05 14:18:56 +0200

    Update sample config

--- config_sample.php
+++ config_sample.php
@@ -1,3 +1,6 @@
-<?Php
-$outpath='/home/tv';
-?>
+<?php
+$config['xmltv_path'] = '/home/xmltv';
+$config['xmltv_default_sub_folder'] = 'xmltv_php';  //Sub folder in the channel folder
+$config['xmltv_alternate_sub_folders'] = array('xmltv');
+
+return $config;
