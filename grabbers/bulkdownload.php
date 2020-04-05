cb2b3caac679ddbc6acad91e2a20df48672c2456
datagutten <datagutten@datagutten.net>
2020-04-05 21:29:08 +0200

    Remove old scripts

--- grabbers/bulkdownload.php
+++
@@ -1,8 +0,0 @@
-<?Php
-$startdate=strtotime($argv[1]);
-for($i=0; $i<=30; $i++)
-{
-	$date=strtotime("+$i days",$startdate);
-	$cmddate=date('Y-m-d',$date);
-	echo "php {$argv[2]}.php $cmddate\n";
-}
