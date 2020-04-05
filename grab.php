544a3c66c76c2c4631f2de3dc354250dd0f67017
datagutten <datagutten@datagutten.net>
2020-04-05 13:15:19 +0200

    Add grabber front script

---
+++ grab.php
@@ -0,0 +1,30 @@
+<?php
+
+use datagutten\xmltv\grabbers;
+
+require 'vendor/autoload.php';
+
+$grabber_class = grabbers\grabbers::grabber($argv[1]);
+
+if(!isset($argv[2]))
+    $start_timestamp=time();
+else
+    $start_timestamp=strtotime($argv[2]);
+
+if(!isset($argv[3])) //If no end timestamp is specified, end today
+    $end_timestamp=$start_timestamp;
+else
+    $end_timestamp=strtotime($argv[3]);
+
+printf("Grabbing from %s to %s", date('c', $start_timestamp), date('c', $end_timestamp));
+
+for($timestamp = $start_timestamp; $timestamp<=$end_timestamp; $timestamp=$timestamp+86400)
+{
+    /**
+     * @var $grabber grabbers\common
+     */
+    $grabber = new $grabber_class;
+    $file = $grabber->grab($timestamp);
+    echo $file."\n";
+    unset($grabber);
+}
