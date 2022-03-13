7287d9c742573e6588c528aff52a70a152034000
datagutten <datagutten@datagutten.net>
2022-03-13 11:27:56 +0100

    Find grabbers from class files

--- grabbers/grabbers.php
+++ grabbers/grabbers.php
@@ -4,7 +4,9 @@
 namespace datagutten\xmltv\grabbers;
 
 
+use FilesystemIterator;
 use InvalidArgumentException;
+use SplFileInfo;
 
 class grabbers
 {
@@ -36,15 +38,17 @@ class grabbers
         ];
 
     /**
+     * Get grabber class
      * @param string $xmltv_id XMLTV DNS-like id
      * @return base\common
      */
-    public static function grabber($xmltv_id)
+    public static function grabber(string $xmltv_id)
     {
-        if(isset(self::$grabbers[$xmltv_id]))
-            return self::$grabbers[$xmltv_id];
+        $grabbers = self::getGrabbers();
+        if (isset($grabbers[$xmltv_id]))
+            return $grabbers[$xmltv_id];
         else
-            throw new InvalidArgumentException('No grabber for '.$xmltv_id);
+            throw new InvalidArgumentException('No grabber for ' . $xmltv_id);
     }
 
     /**
@@ -52,6 +56,30 @@ class grabbers
      */
     public static function getGrabbers()
     {
-        return self::$grabbers;
+        $grabbers = self::$grabbers;
+        $iterator = new FilesystemIterator(__DIR__, FilesystemIterator::SKIP_DOTS | FilesystemIterator::UNIX_PATHS);
+
+        /**
+         * @var $fileInfo SplFileInfo
+         */
+        foreach ($iterator as $fileInfo)
+        {
+            if ($fileInfo->getExtension() != 'php')
+                continue;
+            if ($fileInfo->isDir())
+                continue;
+            if ($fileInfo->getBasename() == 'grabbers.php')
+                continue;
+
+            /** @var base\common $class */
+            $class = 'datagutten\\xmltv\\grabbers\\' . $fileInfo->getBasename('.php');
+            if (property_exists($class, 'xmltv_id') && !empty($class::$xmltv_id))
+            {
+                $id = $class::$xmltv_id;
+                if (!isset($grabbers[$id]))
+                    $grabbers[$id] = $class;
+            }
+        }
+        return $grabbers;
     }
 }
