cbe80f173abbca2b31a5dec5aeace845dde5639e
datagutten <datagutten@datagutten.net>
2024-04-06 18:42:12 +0200

    Boomerang does not exist anymore, remove grabber

--- grabbers/boomerang.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace datagutten\xmltv\grabbers;
-
-class boomerang extends base\vg_no
-{
-    public static string $slug = 'boomerang';
-    public static string $xmltv_id = 'boomerangtv.no';
-}
