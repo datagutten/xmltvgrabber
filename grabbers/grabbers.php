e9966ba199e4516faca636dd3d5c43c0dc22d577
datagutten <datagutten@datagutten.net>
2024-04-20 19:33:50 +0200

    Remove static grabber list

--- grabbers/grabbers.php
+++ grabbers/grabbers.php
@@ -11,32 +11,6 @@ use SplFileInfo;
 class grabbers
 {
     /**
-     * @var base\common[]
-     */
-    public static array $grabbers = [
-        'disneychannel.no' => disney_channel::class,
-        'junior.disneychannel.no' => disney_junior::class,
-        'tvnorge.no' => discovery_tvnorge::class,
-        'fem.no' => discovery_fem_no::class,
-        'voxtv.no' => discovery_vox_no::class,
-        'max.no' => discovery_max_no::class,
-        'discovery.no' => discovery_channel_no::class,
-        'tlc.discovery.no'=> discovery_tlc_no::class,
-        'investigation.discovery.no' => discovery_investigation_no::class,
-        'science.discovery.no' => discovery_science_no::class,
-        'animalplanet.discovery.no' => discovery_animalplanet_no::class,
-        'nrk1.nrk.no' => nrk1::class,
-        'nrk2.nrk.no' => nrk2::class,
-        'nrk3.nrk.no' => nrk3::class,
-        'cbsreality.com' => cbsreality::class,
-        'nrksuper.nrk.no' => nrksuper::class,
-        'tv2.no' => tv2::class,
-        'zebra.tv2.no' => tv2_zebra::class,
-        'nickelodeon.no' => nickelodeon_no::class,
-        'boomerangtv.no' => boomerang::class,
-        ];
-
-    /**
      * Get grabber class
      * @param string $xmltv_id XMLTV DNS-like id
      * @return base\common Class name of grabber class
@@ -55,7 +29,6 @@ class grabbers
      */
     public static function getGrabbers(): array
     {
-        $grabbers = self::$grabbers;
         $iterator = new FilesystemIterator(__DIR__, FilesystemIterator::SKIP_DOTS | FilesystemIterator::UNIX_PATHS);
 
         /**
