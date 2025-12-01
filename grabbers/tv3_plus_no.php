7efb1c0764495b027cfbb465880ba7d7da75d581
Anders Birkenes <datagutten@datagutten.net>
2025-12-01 19:21:34 +0100

    Add grabber for TV3 plus

---
+++ grabbers/tv3_plus_no.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace datagutten\xmltv\grabbers;
+
+class tv3_plus_no extends base\vg_no
+{
+    public static string $slug = 'tv3-plus';
+    public static string $xmltv_id = 'plus.tv3.no';
+}
