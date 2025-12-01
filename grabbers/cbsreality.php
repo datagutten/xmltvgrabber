69fc5a81c7ae7ff6daf77d29e33408b6b307ceb6
Anders Birkenes <datagutten@datagutten.net>
2025-12-01 19:29:02 +0100

    Grab CBS Reality from vg.no

--- grabbers/cbsreality.php
+++ grabbers/cbsreality.php
@@ -3,57 +3,11 @@
 namespace datagutten\xmltv\grabbers;
 
 
-use datagutten\xmltv\tools\build\programme;
-use DOMDocument;
-use DOMXPath;
+use datagutten\xmltv\grabbers\base\vg_no;
 
-class cbsreality extends base\common
+class cbsreality extends vg_no
 {
     public static string $language = 'en';
+    public static string $slug = 'cbs-reality';
     public static string $xmltv_id = 'cbsreality.com';
-
-    function grab(int $timestamp = 0): ?string
-    {
-        if (empty($timestamp))
-            $timestamp = strtotime('midnight');
-        $url = sprintf('https://www.cbsreality.tv/eu/tv_guide.php?date=%s', date('Y-m-d', $timestamp));
-        $data = $this->download_cache($url, $timestamp);
-
-        $dom = new DOMDocument();
-        @$dom->loadHTML($data);
-        $xpath = new DOMXPath($dom);
-        $content = $dom->getElementById('block-wide');
-        $times = $xpath->query('.//div[@class="tvguide-time"]', $content);
-        $shows = $xpath->query('.//div[@class="tvguide-show"]', $content);
-        foreach ($shows as $key => $show)
-        {
-            $time = $times->item($key);
-            $time = trim($time->textContent);
-            $program_start = strtotime($time, $timestamp);
-
-            $programme = new programme($program_start, $this->tv);
-            $sub_title = $xpath->query('./span[@class="sub-title"]', $show);
-            if ($sub_title->length == 1)
-            {
-                $sub_title = trim($sub_title->item(0)->textContent);
-                $programme->sub_title($sub_title);
-                if (preg_match('/Series ([0-9]+), Episode ([0-9]+)/', $sub_title, $matches))
-                {
-                    $programme->series($matches[2], $matches[1]);
-                }
-            }
-            $title = $xpath->query('./a[@class="title"]', $show);
-            $programme->title(trim($title->item(0)->textContent));
-            $description = $xpath->query('./div[@class="tvguide-description"]', $show);
-            $programme->description(trim($description->item(0)->textContent));
-        }
-        if (!empty($programme))
-            return $this->save_file($timestamp);
-        else
-        {
-            $cache_file = $this->local_file($timestamp);
-            unlink($cache_file);
-            throw new exceptions\GrabberException(sprintf('No programs found for date %s', date('Y-m-d', $timestamp)));
-        }
-    }
 }
