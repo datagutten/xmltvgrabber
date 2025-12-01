6da9edd3bcf71e647446223ca1e8f6d926f278d6
Anders Birkenes <datagutten@datagutten.net>
2025-12-01 19:28:22 +0100

    Update slug for BBC crawlers

--- grabbers/bbc_entertainment.php
+++ grabbers/bbc_entertainment.php
@@ -4,6 +4,6 @@ namespace datagutten\xmltv\grabbers;
 
 class bbc_entertainment extends base\tv2no
 {
-    public static string $slug = 'bbcenter';
+    public static string $slug = 'BBCENTER';
     public static string $xmltv_id = 'no.bbcentertainment.no';
 }
--- grabbers/bbc_knowledge.php
+++ grabbers/bbc_knowledge.php
@@ -4,6 +4,6 @@ namespace datagutten\xmltv\grabbers;
 
 class bbc_knowledge extends base\tv2no
 {
-    public static string $slug = 'bbcknow';
+    public static string $slug = 'BBCKNOW';
     public static string $xmltv_id = 'no.bbcknowledge.no';
 }
