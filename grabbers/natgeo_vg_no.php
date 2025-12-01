a7d091a770c3bc792a37b8412e2e63bf713c6565
Anders Birkenes <datagutten@datagutten.net>
2025-12-01 19:30:39 +0100

    Disney junior does not exist anymore

---
+++ grabbers/natgeo_vg_no.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace datagutten\xmltv\grabbers;
+
+
+class natgeo_vg_no extends base\vg_no
+{
+    public static string $xmltv_id = 'natgeo.no';
+    public static string $slug = 'national-geographic';
+    public static string $sub_folder = 'xmltv_vg';
+    public static string $xmltv_alternate_id = 'natgeo.vg.no';
+    public static string $folder_suffix = 'vg';
+}
