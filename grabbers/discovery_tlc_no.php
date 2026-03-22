eb1031612b9a2fbb2261c0f6bdd25549fb8712e7
Anders Birkenes <datagutten@datagutten.net>
2026-03-22 13:55:41 +0100

    Update TLC grabber

--- grabbers/discovery_tlc_no.php
+++ grabbers/discovery_tlc_no.php
@@ -4,8 +4,8 @@
 namespace datagutten\xmltv\grabbers;
 
 
-class discovery_tlc_no extends base\DiscoveryBaseNO
+class discovery_tlc_no extends base\tv2no
 {
-    protected static string $channel_slug = 'tlc-norway';
+    public static string $slug = 'DTRAVEL';
     public static string $xmltv_id = 'tlc.discovery.no';
 }
